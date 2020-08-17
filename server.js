d604bf70524f12df563f7429fa5272a84b362bbd
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-08-17 18:00:59 +0200

    Init + add server && client side starting files

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+
+const tasks = [];
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running at 8000 port...');
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', newTask => {
+    tasks.push(newTask);
+    socket.broadcast.emit('addTask', newTask);
+  });
+
+  socket.on('removeTask', data => {
+    console.log(`remove task of id=${data}`);
+    const getTask = tasks.find(task => task.id == data);
+    const taskIndex = tasks.indexOf(getTask);
+    tasks.splice(taskIndex,1);
+    socket.broadcast.emit('removeTask', data);
+  });
+});
